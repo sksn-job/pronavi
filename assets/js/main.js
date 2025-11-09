9f4eca4a1cd890cf296408aa76826a7bf3912569
早和子 <sako@hayawakonoMacBook-Pro.local>
2025-11-09 23:17:59 +0900

    assetsフォルダとindex.html更新、topのslick上のキャッチコピー寂しかったので入れてみました、いい感じで画像できたら、良いんでは？と、、

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,55 @@
-//test
+$(document).ready(function () {
+  $(".slick_slider").slick({
+    autoplay: true,
+    autoplaySpeed: 3000,
+    arrows: true,
+    dots: true,
+    infinite: true,
+  });
+});
+
+$(function () {
+  /*=================================================
+  ハンバーガーメニュー
+  解説は、「中級編：ストアサイト（インテリア）」参照
+  ===================================================*/
+  $(".toggle_btn").on("click", function () {
+    $(this).toggleClass("open");
+    $("#nav, #mask").toggleClass("open"); // ← navにもopenを付ける
+  });
+
+  // #maskのエリアをクリックした時にメニューを閉じる
+  $("#mask,#nav a").on("click", function () {
+    $(".toggle_btn, #nav, #mask").removeClass("open");
+  });
+
+  /*=================================================
+  トップに戻る
+  ===================================================
+  let pagetop = $("#to_top");
+  // 最初に画面が表示された時は、トップに戻るボタンを非表示に設定
+  pagetop.hide();
+
+  // スクロールイベント（スクロールされた際に実行）
+  $(window).scroll(function () {
+    // スクロール位置が700pxを超えた場合
+    if ($(this).scrollTop() > 700) {
+      // トップに戻るボタンを表示する
+      pagetop.fadeIn();
+
+      // スクロール位置が700px未満の場合
+    } else {
+      // トップに戻るボタンを非表示にする
+      pagetop.fadeOut();
+    }
+  });
+
+  // クリックイベント（ボタンがクリックされた際に実行）
+  pagetop.click(function () {
+    // 0.5秒かけてページトップへ移動
+    $("body,html").animate({ scrollTop: 0 }, 500);
+
+    // イベントが親要素へ伝播しないための記述
+    return false;
+  });*/
+});
