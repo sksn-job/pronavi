f355591908125beef22d75d2d39a396c3af33eeb
mayumi <mayu.ib.1211@gmail.com>
2025-11-23 07:01:21 +0900

    今日の修正をプッシュ

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,6 +12,7 @@ $(document).ready(function () {
 
 //スムーススクロール
 $('a[href^="#"]').on("click", function (e) {
+  if ($(this).attr("id") === "to_top") return;
   e.preventDefault(); // デフォルトのジャンプをキャンセル
   var target = $(this.getAttribute("href")); // 対象要素
 
@@ -162,30 +163,32 @@ $(function () {
 
 /*=================================================
   トップに戻る
-  ===================================================
-  let pagetop = $("#to_top");
-  // 最初に画面が表示された時は、トップに戻るボタンを非表示に設定
-  pagetop.hide();
+  ===================================================*/
+let pagetop = $("#to_top");
+// 最初に画面が表示された時は、トップに戻るボタンを非表示に設定
+pagetop.hide();
 
-  // スクロールイベント（スクロールされた際に実行）
-  $(window).scroll(function () {
-    // スクロール位置が700pxを超えた場合
-    if ($(this).scrollTop() > 700) {
-      // トップに戻るボタンを表示する
-      pagetop.fadeIn();
+// スクロールイベント（スクロールされた際に実行）
+$(window).scroll(function () {
+  // スクロール位置が700pxを超えた場合
+  if ($(this).scrollTop() > 700) {
+    // トップに戻るボタンを表示する
+    pagetop.fadeIn();
 
-      // スクロール位置が700px未満の場合
-    } else {
-      // トップに戻るボタンを非表示にする
-      pagetop.fadeOut();
-    }
-  });
+    // スクロール位置が700px未満の場合
+  } else {
+    // トップに戻るボタンを非表示にする
+    pagetop.fadeOut();
+  }
+});
 
-  // クリックイベント（ボタンがクリックされた際に実行）
-  pagetop.click(function () {
-    // 0.5秒かけてページトップへ移動
-    $("body,html").animate({ scrollTop: 0 }, 500);
+// クリックイベント（ボタンがクリックされた際に実行）
+pagetop.click(function () {
+  // 0.5秒かけてページトップへ移動
+  $("body,html").animate({ scrollTop: 0 }, 500);
 
-    // イベントが親要素へ伝播しないための記述
-    return false;
-  });*/
+  // イベントが親要素へ伝播しないための記述
+  return false;
+});
+
+
